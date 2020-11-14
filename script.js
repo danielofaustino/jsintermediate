f1f5e95f669e1b2b5b03c229e81d01a5e8ada500
danielofaustino <danieloliveirafaustino@gmail.com>
2020-11-14 16:12:24 -0300

    using this

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 // variable to receive true or false when button is clicked
 let flag = false
 
-// function to change the color property in style of #p1 element
-const changeColor= ()=>{
-
-   // getting element that has p1 in id name 
-   let paragraph = document.getElementById("p1")
+const changeColor= (button)=>{
 
     // logic to test if button was clicked
     if(flag == false){
 
         //changing style property
-        paragraph.style ="color:blue"
+        button.style ="color:blue"
         //changing flag status
         flag = true
 
    }else if(flag == true){
 
-        paragraph.style ="color:red"
+        button.style ="color:red"
         flag = false
 
    }
