14b33270ad310f7e9dc6b0a26776f4131b5d7cf7
danielofaustino <danieloliveirafaustino@gmail.com>
2020-11-14 16:26:38 -0300

    counter

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // variable to receive true or false when button is clicked
 let flag = false
+// variable to receive how many times the button was changed the color
+let cont = 1
 
 const changeColor= (button)=>{
 
+    let Paragraph = document.getElementById('p1')
     // logic to test if button was clicked
     if(flag == false){
 
@@ -10,12 +13,13 @@ const changeColor= (button)=>{
         button.style ="color:blue"
         //changing flag status
         flag = true
+        Paragraph.innerText = cont++
 
    }else if(flag == true){
 
         button.style ="color:red"
         flag = false
-
+        Paragraph.innerText = cont++
    }
        
     
