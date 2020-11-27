9b0146276c0264cc3adfb96bc4678560d330830f
danielofaustino <danieloliveirafaustino@gmail.com>
2020-11-27 17:21:53 -0300

    Creating Events using "AddEventListener"

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
-// variable to receive true or false when button is clicked
-let flag = false
-// variable to receive how many times the button was changed the color
-let cont = 1
 
-const changeColor= (button)=>{
+window.onload = function(){
 
-    let Paragraph = document.getElementById('p1')
-    // logic to test if button was clicked
-    if(flag == false){
-
-        //changing style property
-        button.style ="color:blue"
-        //changing flag status
-        flag = true
-        Paragraph.innerText = cont++
-
-   }else if(flag == true){
-
-        button.style ="color:red"
-        flag = false
-        Paragraph.innerText = cont++
-   }
-       
+    let button = document.getElementsByTagName("button")[0]
+    let Paragraph = document.getElementsByTagName('p')[0]
+    let input = document.getElementsByTagName('input')[0]
+    let header = document.getElementsByTagName("h1")[0]
     
+
+    // variable to receive true or false when button is clicked
+    let flag = false
+    // variable to receive how many times the button was changed the color
+    let cont = 1
+        
+
+    // functions
     
-}
+    const changeH1= () =>{
+    
+        header.innerText = input.value
+    } 
 
+    const changeColor= ( )=>{
 
-const changeH1= (i) =>{
-    let h1 = document.getElementsByTagName("h1")[0]
-    h1.innerText = i.value
+        
+        // logic to test if button was clicked
+        if(flag == false){
+    
+            //changing style property
+            button.style ="color:blue"
+            //changing flag status
+            flag = true
+            Paragraph.innerText = cont++
+    
+       }else if(flag == true){
+    
+            button.style ="color:red"
+            flag = false
+            Paragraph.innerText = cont++
+       }      
+     
+        
+    }
+
+    // Events
+    button.addEventListener("click",changeColor)
+    input.addEventListener('input',changeH1)
 }
